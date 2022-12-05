fcbaa3ece8cd8a3da84f1f725d1b18e81afe12d1
Anitha-najani <anithanajani26@gmail.com>
2022-12-05 11:53:00 +0530

    react-components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 /*import ClassComponent from './ClassComponent'
 import ClassComponent2 from './ClassComponent2'
 function App(){
@@ -16,23 +15,36 @@ function App(){
 }
 export default App*/
 
-
-
 //import PropsExample from './PropsExample'
 //import FuncPropExample from './FuncPropExample'
 //import StateExample from "./StateExample"
-import Events from './Events'
+//import Events from './Events'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import Navbar from './components/Navbar'
+import NotFound from './components/NotFound'
 function App(){
   return(
       <div>
+        <BrowserRouter>
+        <Navbar/>
+           <Routes>
+               <Route path="/" element={<Home/>}/>
+               <Route path="/about" element={<About/>}/>
+               <Route path="/contact" element={<Contact/>}/>
+               <Route path="*" element={<NotFound/>}/>
+           </Routes>
+        </BrowserRouter>
           {/*<h1> This is about props example</h1>
           <PropsExample name="lalitha" age="20"/>
           <PropsExample name="manasa" age="20"/>
           <PropsExample name="navya" age="20"/>*/}
           {/*<FuncPropExample framework="react" number="2" name="lalli" age="20"/>
           
-          <StateExample/>*/}
-          <Events/>
+          <StateExample/>
+          <Events/>*/}
       </div>
     )
 }
